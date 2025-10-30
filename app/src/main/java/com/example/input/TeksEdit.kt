1eaf4589220ec2e7a8ed5bed7936ae3160209a05
taznimt <taznimanwar06@gmail.com>
2025-10-30 14:12:26 +0700

    Add horizontal divider with padding and custom thickness

--- app/src/main/java/com/example/input/TeksEdit.kt
+++ app/src/main/java/com/example/input/TeksEdit.kt
@@ -118,5 +118,12 @@ fun FormDataDiri(modifier: Modifier = Modifier) {
         ) {
             Text(stringResource(R.string.submit))
         }
+        HorizontalDivider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
     }
 }
