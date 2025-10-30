77961b9f4def6a09078a992e1b061eff35b2c3ad
taznimt <taznimanwar06@gmail.com>
2025-10-30 09:25:43 +0700

    Add OutlinedTextField for address input and HorizontalDivider with padding

--- app/src/main/java/com/example/input/TeksEdit.kt
+++ app/src/main/java/com/example/input/TeksEdit.kt
@@ -3,7 +3,10 @@ package com.example.input
 import android.text.Layout
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -12,6 +15,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 
 @Composable
@@ -54,6 +59,23 @@ fun FormDataDiri(modifier: Modifier){
                 }
             }
         }
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(width = 250.dp),
+            label = {Text(text = "Alamat Lengkap")},
+            onValueChange = {
+                textAlamat = it
+            }
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(30dp),
+                top = dimensionResource(
+                    id = 30dp
+                )
+            ),
+        )
 
     }
 
