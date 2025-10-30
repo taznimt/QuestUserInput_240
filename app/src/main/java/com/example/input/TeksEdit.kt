c41e81466c11cbe743b937b8757abd93684d40e7
taznimt <taznimanwar06@gmail.com>
2025-10-30 08:54:27 +0700

    Menambahkan variabel state textNama dengan remember dan mutableSetOf

--- app/src/main/java/com/example/input/TeksEdit.kt
+++ app/src/main/java/com/example/input/TeksEdit.kt
@@ -1,9 +1,10 @@
 package com.example.input
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import java.lang.reflect.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier){
-
+    var textNama by remember { mutableSetOf(value = "") }
 }
