ddfa740b231904e1bf5e4fae134c5bfd4881cb3b
taznimt <taznimanwar06@gmail.com>
2025-11-01 10:24:43 +0700

    apply full size modifier to composable

--- app/src/main/java/com/example/input/TeksEdit.kt
+++ app/src/main/java/com/example/input/TeksEdit.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -159,5 +160,6 @@ fun FormDaftar(modifier: Modifier = Modifier) {
 
     Box(
         modifier = modifier
+            .fillMaxSize()
     )
 }
